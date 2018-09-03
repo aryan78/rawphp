94f51c3768ee204ab5519f2b6455c16a32e1b46a
Aryan Sitaula <aryan.sitaula@yahoo.com>
2018-09-03 13:26:14 +0545

    Bug #1 fix

--- person-load.php
+++ person-load.php
@@ -1,19 +1,33 @@
 <?php
-require 'dbconn.php'; 
-?>
-<?php
+    require 'dbconn.php'; 
+    
     $name_file = fopen('person-name.txt', 'r') or die("Unable to open file!");
     while (!feof($name_file)) {
-       $name = fgets($name_file);
-       echo $name;
-       $sql = "INSERT INTO person VALUES ('$name')";
-       echo "$sql";
-       if ($conn->query($sql) === TRUE) {
-           echo "Record updated successfully";
-       } else {
-           echo "Error updating record: " . $conn->error;
+       $name = stream_get_line ( $name_file , 100 , "\n");
+       if ($name != "") {
+           $sql = "INSERT INTO person VALUES ('$name')";
+           if ($conn->query($sql) === TRUE) {
+               echo "Record updated successfully\n";
+           } else {
+               echo "Error updating record: " . $conn->error;
+           }
        }
     }
-    
+
     $conn->close();
+
+/* 
+ * Return:
+ *
+ * True if string is empty
+ * False if string is not empty
+ */
+function checkEmptyString($str)
+{
+  $flag = True;
+
+  // Test code here
+
+  return $flag;
+}
 
